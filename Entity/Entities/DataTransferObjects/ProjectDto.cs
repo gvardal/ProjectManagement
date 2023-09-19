d2da655268de150c731368a622a56fa89e4c987f
Giray VARDAL <gvardal@yahoo.com>
2023-09-19 11:18:37 +0300

    Add Create Main Project Dialog

--- Entity/Entities/DataTransferObjects/ProjectDto.cs
+++ Entity/Entities/DataTransferObjects/ProjectDto.cs
@@ -1,11 +1,20 @@
-﻿namespace Entities.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.DataTransferObjects
 {
     public class ProjectDto
     {
         public int ProjectId { get; set; }
+
+        [Required(ErrorMessage ="Project Name cannot be empty")]
         public string TaskName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Project Start Date cannot be empty")]
         public DateTime? StartDate { get; set; }
+
+        [Required(ErrorMessage = "Project End Date cannot be empty")]
         public DateTime? EndDate { get; set; }
+
         public string? Duration { get; set; }
         public int Progress { get; set; }
         public int? ParentId { get; set; }
