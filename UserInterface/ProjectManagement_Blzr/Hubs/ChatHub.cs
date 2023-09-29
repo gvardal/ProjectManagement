8f78a447dce4e7dffaf7c7cec284eaf9393a39aa
Giray VARDAL <gvardal@yahoo.com>
2023-09-29 14:00:51 +0300

    Add If show when received any record kanban card

---
+++ UserInterface/ProjectManagement_Blzr/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ProjectManagement_Blzr.Hubs;
+
+public class ChatHub : Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync(method: "ReceiveMessage", user, message);
+    }
+}
---
+++ UserInterface/ProjectManagement_Blzr/Hubs/KanbanHub.cs
@@ -0,0 +1,13 @@
+﻿using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ProjectManagement_Blzr.Hubs
+{
+    public class KanbanHub : Hub
+    {
+        public Task ChangeStatus(KanbanCardDto kanbanCard)
+        {
+            return Clients.All.SendAsync(method: "ChangedKanbanCard", kanbanCard);
+        }
+    }
+}
