5c65379df9957aff3d9283fddc093b5f616f41c8
Giray VARDAL <gvardal@yahoo.com>
2023-10-10 17:53:21 +0300

    Preparing EventBus mechanizm

---
+++ EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+        [JsonProperty]
+        public Guid Id { get; private set; }
+
+        [JsonProperty]
+        public DateTime CreatedDate { get; private set; }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid ıd, DateTime createdDate)
+        {
+            Id = ıd;
+            CreatedDate = createdDate;
+        }
+
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate= DateTime.Now;
+        }
+    }
+}
---
+++ EventBus/EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,60 @@
+﻿using EventBus.Base;
+using EventBus.Base.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace EventBus.RabbitMQ
+{
+    public class EventBusRabbitMQ : BaseEventBus
+    {
+        PersistentConnection persistentConnection;
+        private readonly IConnectionFactory connectionFactory;
+
+
+        public EventBusRabbitMQ(IServiceProvider serviceProvider, EventBusConfig config) : base(serviceProvider, config)
+        {
+            if (config.Connection != null)
+            {
+                var connJson = JsonConvert.SerializeObject(config.Connection, new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                });
+                connectionFactory = JsonConvert.DeserializeObject<ConnectionFactory>(connJson);
+            }
+            else
+            {
+                connectionFactory = new ConnectionFactory();
+            }
+
+            persistentConnection = new PersistentConnection(connectionFactory, config.ConnectionRetryCount);
+        }
+
+        public override void Publish(IntegrationEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Subscribe<T, TH>()
+        {
+            var eventName = typeof(T).Name;
+            eventName = ProccessEventName(eventName);
+            if (!SubsManager.HasSubscriptionForEvent(eventName))
+            {
+
+            }
+        }
+
+        public override void UnSubscribe<T, TH>()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IModel CreateConsumerChannel()
+        {
+            if (true)
+            {
+
+            }
+        }
+    }
+}
